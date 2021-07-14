2c9b79d7874a925da979ccf6614af39e3886d9ce
hansani <hansanimadushika.996@gmail.com>
2021-07-14 12:28:45 +0530

    first commit

---
+++ src/lk/swlc/snakegame/view/snakeBoard.java
@@ -0,0 +1,19 @@
+package lk.swlc.snakegame.view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class snakeBoard extends JPanel implements ActionListener,Runnable {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
