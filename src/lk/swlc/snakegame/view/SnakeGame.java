1caa1aa8d1d6db63a5335837844e1cb302807ee4
hansani <hansanimadushika.996@gmail.com>
2021-07-16 10:33:04 +0530

    snakeGame start

---
+++ src/lk/swlc/snakegame/view/SnakeGame.java
@@ -0,0 +1,9 @@
+package lk.swlc.snakegame.view;
+
+import javax.swing.*;
+
+public class SnakeGame  extends JFrame {
+
+    private static final long serialVID = 6678292058307426314L;
+    private static final long frameTime = 100;
+}
